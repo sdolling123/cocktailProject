e52a7e29efca6abe68e7d63ef4caaeb0b89472f6
Sam <samdolling@me.com>
2024-11-29 17:22:37 -0600

    delete, messages, modals, and utilities

--- ingredients/urls.py
+++ ingredients/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import IngredientListView, IngredientDetailView, IngredientCreateView, IngredientEditView
+from .views import IngredientListView, IngredientDetailView, IngredientCreateView, IngredientEditView, ConfirmIngredientDeleteView, IngredientDeleteView
 
 urlpatterns = [
     path('',IngredientListView.as_view(),name='ingredient-list'),
@@ -7,4 +7,6 @@ urlpatterns = [
     path('edit/<int:pk>/',IngredientEditView.as_view(),name='ingredient-edit'),
     path('delete/<int:pk>/',IngredientEditView.as_view(),name='ingredient-delete'),
     path('create/',IngredientCreateView.as_view(),name='ingredient-create'),
+    path('delete_ingredient/<int:pk>/',IngredientDeleteView.as_view(),name='delete-ingredient'),
+    path('confirm-ingredient-delete/<int:pk>/',ConfirmIngredientDeleteView.as_view(),name='confirm-ingredient-delete'),
 ]
--- ingredients/views.py
+++ ingredients/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from ingredients.models import Ingredient
 from .forms import IngredientForm
 
@@ -36,4 +36,21 @@ class IngredientEditView(UpdateView):
     form_class = IngredientForm
     template_name = 'ingredients/ingredient_create.html'
     success_url = reverse_lazy('ingredient-list')
-    
+
+class ConfirmIngredientDeleteView(DetailView):
+    model = Ingredient
+    context_object_name = 'ingredient'
+    template_name = 'ingredients/partials/ingredient_confirm_delete.html'
+
+class IngredientDeleteView(DeleteView):
+    success_url = reverse_lazy('ingredient-list')
+
+    def post(self, request, pk):
+        ingredient = get_object_or_404(Ingredient, pk=pk)
+        ingredient_name = ingredient.name  # Capture name for a more personalized message
+        ingredient.delete()
+        
+        # Add a success message
+        messages.success(request, f'"{ingredient_name}" successfully deleted.')
+        
+        return redirect(self.success_url)
